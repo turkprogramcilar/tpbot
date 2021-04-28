fe49b74d8c902b8b276e4a85375b4384c31aa54e
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-04-28 15:14:21 +0000

    initial anon-vote

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const bot = require("./webserver/server.js");
-require("./discordbot/bot.js")
-    .set_sendallF(bot.send_all);
+const webserver =
+process.env.WEBSERVER == null || process.env.WEBSERVER != false
+? require("./webserver/server.js")
+: null;
+
+const bot = require("./discordbot/bot.js")
+
+if (webserver != null)
+bot.set_sendallF(webserver.send_all);
