95d40c3c419226edc6c65a857fb8508343cf8b0d
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-06-05 02:08:29 +0300

    Create arena.js

---
+++ discordbot/arena.js
@@ -0,0 +1,56 @@
+let alive = []
+exports.frequency = 10;
+exports.create = (dc, msg, name, id, hp) => {
+    if (alive.length>0) return null;
+    ch = msg.guild.channels.cache.get(cid_arena);
+    if (ch) ch.send(embed(dc, name, id, hp, hp)).then((msg) => {
+        alive.push({
+            name: name,
+            id: id,
+            msg: msg, 
+            mhp: hp,
+             hp: hp,
+            lasthit: +new Date(),
+            dmgdone: {},
+        });
+    });
+}
+exports.hit = (dc, msg, gm=false,gmdmg=0) => {
+    if (alive.length==0) return;
+    dmg=Math.random()*100|0;
+    if (gm) dmg=gmdmg;
+    monster=alive[0];
+    uname=msg.author.username;
+    if (monster.dmgdone[uname])
+        monster.dmgdone[uname] += dmg;
+    else
+        monster.dmgdone[uname] = dmg;
+    monster.hp-=dmg;
+    msg.delete();
+    if (monster.hp <= 0) {
+        monster.lasthit=0;
+        alive=[];
+    }
+    now = new Date(); passed = (now - monster.lasthit) / 1000 | 0;
+    if (passed >= exports.frequency) {
+        //update
+        sorted=Object.entries(monster.dmgdone).sort((a,b)=>b[1]-a[1])
+        m=monster;
+        m.msg.edit(embed(dc, m.name,m.id,m.hp,m.mhp,sorted.splice(0,3),sorted.splice(-1,1)[0]));
+        monster.lasthit = now;
+    }
+}
+const embed = (dc, name, id, hp, mhp, top=[], last='') => {
+    t1=top[0]??"-"; t2=top[1]??"-"; t3=top[2]??"-"; t4=last??"-";
+    return new dc.MessageEmbed()
+    .setColor('#F80000')
+    .setTitle(`MEGA ${name} ORTAYA ÇIKTI!`)
+    .setDescription(`Can: ${hp}/${mhp}`)
+    .addField('Birinci', t1, true)
+    .addField('İkinci',  t2, true)
+    .addField('Üçüncü',  t3, true)
+    //.addField('Sonuncu', t4, true)
+	.setThumbnail(`https://cdn.discordapp.com/emojis/${id}.png`)
+    .setTimestamp()
+	.setFooter('%vur komutu ile düşmana saldır. Daha fazla hasar vermek için saldırını emojiler ile desteklendir!');
+}
