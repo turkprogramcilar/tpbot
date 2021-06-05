a7805844807e6c39f04f39e34c708adbf0a42313
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-06-05 15:28:59 +0300

    add title system for hp levels

--- discordbot/arena.js
+++ discordbot/arena.js
@@ -41,16 +41,32 @@ exports.hit = (dc, msg, gm=false,gmdmg=0) => {
     }
 }
 const embed = (dc, name, id, hp, mhp, top=[], last='') => {
+
+    // boyle yapmayinca bir sebepten dolayi calismiyordu belki de simdi t1..t4
+    // yerine karsiliklarini yazinca calisabilir.
     t1=top[0]??"-"; t2=top[1]??"-"; t3=top[2]??"-"; t4=last??"-";
+
     return new dc.MessageEmbed()
-    .setColor('#F80000')
-    .setTitle(`MEGA ${name} ORTAYA ÇIKTI!`)
-    .setDescription(`Can: ${hp}/${mhp}`)
-    .addField('Birinci', t1, true)
-    .addField('İkinci',  t2, true)
-    .addField('Üçüncü',  t3, true)
-    //.addField('Sonuncu', t4, true)
-	.setThumbnail(`https://cdn.discordapp.com/emojis/${id}.png`)
-    .setTimestamp()
-	.setFooter('%vur komutu ile düşmana saldır. Daha fazla hasar vermek için saldırını emojiler ile desteklendir!');
+        .setColor('#F80000')
+        .setTitle(title(name, hp,mhp))
+        .setDescription(`Can: ${hp}/${mhp}`)
+        .addField('Birinci', t1, true)
+        .addField('İkinci',  t2, true)
+        .addField('Üçüncü',  t3, true)
+        //.addField('Sonuncu', t4, true) bu calismiyor anlamadim.
+        .setThumbnail(`https://cdn.discordapp.com/emojis/${id}.png`)
+        .setTimestamp()
+        .setFooter('%vur komutu ile düşmana saldır. Daha fazla hasar vermek için saldırını emojiler ile desteklendir!');
+}
+
+const title = (name, hp, mhp) => {
+    
+    // pc=percentage
+    pc=hp/mhp;
+         if (pc <= 0) return `MEGA ${name} MAĞLUP EDİLDİ. TEBRİKLER! PUAN TABLOSU:`;
+    else if (pc < .1) return `MEGA ${name} SON DEMLERİNİ YAŞIYOR!`;
+    else if (pc < .3) return `MEGA ${name} ÖLMEK ÜZERE!!!`;
+    else if (pc < .6) return `MEGA ${name} TP ÜYELERİ KARŞISINDA ZORLANIYOR!`;
+    else if (pc < .9) return `MEGA ${name} TP ÜYELERİ TARAFINDAN KUŞATILDI.`;
+    else              return `MEGA ${name} MEYDANA ÇIKTI.`;
 }
