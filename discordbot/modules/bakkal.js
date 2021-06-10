6cc85eaa8e6037f5d195d8a274b8b475adde8f69
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-06-10 16:03:22 +0300

    improve item lookup command with embed icons

--- discordbot/modules/bakkal.js
+++ discordbot/modules/bakkal.js
@@ -3,7 +3,9 @@ const db    = require("./../mongodb.js");
 const parse = require("./../cmdparser.js");
 const fs    = require("fs").promises;
 
-const iconpath = './../icons';
+const Discord = require('discord.js');
+
+const iconpath = './discordbot/icons';
 
 let state = undefined;
 exports.init = (refState) => state = refState;
@@ -18,14 +20,37 @@ exports.on_event = async (evt, args) => {
             && parse.is(msg, "bk ")) {
             // id ile item bilgisi sorgulama
             parse.i_arg(msg, async id => {
-                const [m, i, p] = await Promise.all([
-                    msg.channel.send("..."),
+                const [i, p] = await Promise.all([
                     db.get_item(id),
-                    fs.read(`${iconpath}/${id}.png`)
-                        .catch(async ()=> fs.read(`${iconpath}/undefined.png`))
+                    fs.readFile(`${iconpath}/${id}.png`)
+                        .catch(async ()=> fs.readFile(`${iconpath}/undefined.png`))
                         //.catch() buda yoksa coksun bot napalim.
                 ]);
-                await m.edit(parse.tqs(JSON.stringify(i, null, '\t'),'json'));
+                
+                const title = (i?.strName??"Item not found");
+                const price = i?.BuyPrice??0;
+                let embedded = new Discord.MessageEmbed()
+                    //.setTitle(i?.strName??"Item not found")
+                    .setThumbnail(`attachment://icon.png`);
+                if (i?.strDesc) embedded = embedded.addField('`'+title+'`', parse.tqs(i.strDesc))
+                if (i) {
+                    delete i["_id"]; delete i["id"];
+                    delete i["strName"]; delete i["strDesc"];
+                    delete i["IconID"]; delete i["BuyPrice"];
+                    for (const k of Object.keys(i)) {
+                        if (!i[k] || i[k].toString()=='0')
+                            delete i[k];
+                    }
+                }
+                
+                await msg.channel.send({
+                    files: [{
+                        attachment: p,
+                        name:'icon.png'
+                    }],
+                    embed: embedded
+                        .addField(`Buy price: ${price}`, parse.tqs(JSON.stringify(i??{},null,'\t'),'json'))
+                });
             });
         }
 
