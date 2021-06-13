631b61b643e4e3dd50e1b30eb8dafd00294322ff
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-06-13 20:24:50 +0300

    generalize sendwarn and add user cooldown

--- discordbot/cmdparser.js
+++ discordbot/cmdparser.js
@@ -18,7 +18,9 @@ exports.json_pretty = (str) => exports.tqs(JSON.stringify(str,null,'\t'), 'json'
 exports.tq =  (str,format='') => { return '```'+format+'\n'+str+'```'; }
 // wrap with triple quote + safe (2000 char limit)
 exports.tqs = (str,format='') => { return exports.tq((str?.toString() ?? "").substr(0,2000-1-6-format.length-1),format); }
-exports.send_tqswarn = async (msg, str) => await msg.channel.send(exports.tqs("warning: "+str,"fix"));
+exports.send_tqswarn = async (msg, str) => await send_tqs_custom(msg, str, "fix", "warning");
+exports.send_uwarn = async (msg, str) => await send_tqs_custom(msg, str, "CSS", "uyarı");
+const send_tqs_custom = async (msg, str, format, title) => await msg.channel.send(exports.tqs(title+": "+str,format));
 exports.mention = (msg, f, fe=()=>{}) => { exports.regex_arg(msg, f, fe, x=>x, /^<@!?([0-9]+)>/, 1); }
 exports.mention_else_self = (msg, f) => { exports.mention(msg, f, ()=>f(msg.author.id))}
 exports.r_arg = (msg, regex, f, fe=()=>{}) => { exports.regex_arg(msg, f, fe, x=>x, regex); }
@@ -43,14 +45,20 @@ exports.cooldown_global_debug_print = (state, key, cds) => {
     const ret = undefined==b4r || new Date(b4r.getTime()+cds*msS) <= now;
     console.log({"b4r": b4r, "now": now, "diff": new Date(b4r.getTime()+cds*msS).getSeconds(), "ret": ret});
 }
-exports.cooldown_global = (state, key, cds=5, fdo=()=>{}, felse=()=>{}) => {
+const cooldown = (state, key, get, set, cds, fdo, felse) => {
     const msS = 1000; const now = new Date();
-    const b4r = state.cooldown.global[key];
+    const b4r = get();
     const ret = undefined==b4r || new Date(b4r.getTime()+cds*msS) <= now;
     if (ret) {
-        state.cooldown.global[key] = now;
+        set(now);
         fdo(state, key, cds);
     }
     else felse(state, key, cds);
     return ret;
+}
+exports.cooldown_global = (state, key, cds=5, fdo=()=>{}, felse=()=>{}) =>
+    cooldown(state, key, ()=>state.cooldown.global[state], s=>state.cooldown.global[state]=s, cds, fdo, felse);
+exports.cooldown_user = (state, id, key, cds=5, fdo=()=>{}, felse=()=>{}) => {
+    if (!state.cooldown.users[id]) state.cooldown.users[id]={};
+    return cooldown(state, key, ()=>state.cooldown.users[id][key], s=>state.cooldown.users[id][key]=s, cds, fdo, felse);
 }
