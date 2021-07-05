b24ceefb1a7bcd5bc34d1ce7a8c1b063176f930d
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-07-05 20:39:05 +0300

    fix undefined errors

--- discordbot/module.ts
+++ discordbot/module.ts
@@ -131,7 +131,7 @@ export class dcmodule {
         return this.get_raw_ms()[MS_DCUSERS];
     }
     protected get_module_state_user(user_id : string) : module_user_state {
-        return this.get_raw_ms()[MS_DCUSERS][user_id] ?? {};
+        return ((this.get_raw_ms() ?? {})[MS_DCUSERS] ?? {})[user_id] ?? {};
     }
     protected get_module_state_user_value(user_id : string, key : string) : user_state_value {
         return this.get_module_state_user(user_id)[key];
@@ -176,7 +176,7 @@ export class dcmodule {
         return promise;
     }
     private get_raw_ms() {
-        return this.state.cache.module[this.module_name];
+        return (this.state?.cache?.module ?? {})[this.module_name];
     }
     private set_raw_ms(value : any) {
         this.state.cache.module[this.module_name] = value;
