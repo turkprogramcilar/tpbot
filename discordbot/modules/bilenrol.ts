5b9ce99c87aeecedce50258070129823885cf16d
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-07-25 18:45:43 +0300

    remove unnecessary promises

--- discordbot/modules/bilenrol.ts
+++ discordbot/modules/bilenrol.ts
@@ -12,13 +12,12 @@ const this_dcmodule = class bilenrol extends dcmodule {
     
     public async after_init(){
         
-        const guilds = await Promise.all(this.get_client().guilds.cache.map(g => g.fetch()));
-        const guild = guilds.filter(g => g.channels.cache.get(this.ch_roles) != undefined);
-        if (guild.length == 0) {
+        const guild = this.get_client().guilds.cache.filter(g => g.channels.cache.get(this.ch_roles) != undefined);
+        if (guild.size == 0) {
             console.error("guild length == 0");
             return;
         }
-        const channel = guild[0].channels.cache.get(this.ch_roles)!;
+        const channel = guild.first()!.channels.cache.get(this.ch_roles)!;
         if (channel?.isText()) {
             const text_channel = channel as TextChannel;
             await text_channel.messages.fetch();
