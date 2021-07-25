4b356d7fd0bb849dc634e489c5ec10212dc83a87
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-07-25 19:45:50 +0300

    fix mongodb error

--- discordbot/mongodb.js
+++ discordbot/mongodb.js
@@ -84,7 +84,7 @@ const db_exp_differ = (id, diff) => async (db) => {
     finally close the connection if succesful.
  */
 exports.set_module_state = async (module, json) => db_do(db_set(moduletb, module, { "json": json }));
-exports.get_module_state = async (module) => db_do(db_get(moduletb, module), x=>x.json);
+exports.get_module_state = async (module) => db_do(db_get(moduletb, module), x=>x?.json);
 //
 exports.get_cards = async () => db_do(db_get_all(cardstb));
 exports.get_items = async () => db_do(db_get_all(itemstb));
