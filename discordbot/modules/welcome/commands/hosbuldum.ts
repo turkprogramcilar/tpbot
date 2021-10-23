c7619d65c56545e777560e440fa648d3a2d3ae91
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-10-23 18:07:00 +0300

    start to develop welcome module

---
+++ discordbot/modules/welcome/commands/hosbuldum.ts
@@ -0,0 +1,19 @@
+import { CommandInteraction } from "discord.js";
+
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ApplicationCommandPermissionTypes } from "discord.js/typings/enums";
+import { dcmodule } from "../../../module";
+
+export const data = new SlashCommandBuilder()
+    .setName('hosbuldum')
+    .setDescription('Türk programcılar onay sistemini başlatır')
+    .setDefaultPermission(false);
+
+export const permissions = [
+    { id: dcmodule.role_id_hosgeldiniz, type: ApplicationCommandPermissionTypes.ROLE, permission: true, },
+    { id: dcmodule.role_id_kidemli,     type: ApplicationCommandPermissionTypes.ROLE, permission: true, },
+];
+
+export async function execute(interaction : CommandInteraction) {
+    await interaction.reply({ content: 'Pong!', ephemeral: true});
+}
