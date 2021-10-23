aa31fb283bace7ebbcbe2b0bbd80654de32fa955
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-10-23 22:41:58 +0300

    implement button on hosbuldum command

--- discordbot/modules/welcome/commands/hosbuldum.ts
+++ discordbot/modules/welcome/commands/hosbuldum.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
 
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { ApplicationCommandPermissionTypes } from "discord.js/typings/enums";
@@ -16,5 +16,19 @@ export const permissions = [
 ];
 
 export async function execute(interaction : CommandInteraction) {
-    await interaction.reply({ content: 'Pong!', ephemeral: true});
+    
+    const row = new MessageActionRow()
+        .addComponents( new MessageButton()
+            .setCustomId('primary')
+            .setLabel('Primary')
+            .setStyle('PRIMARY'),
+        );
+
+    const embed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org')
+        .setDescription('Some description here');
+
+    await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
 }
