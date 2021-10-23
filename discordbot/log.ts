c8dd1cf85dd8e6dc13a9353317bc56e127946fee
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-10-23 22:25:27 +0300

    improve logging with allowing user details

--- discordbot/log.ts
+++ discordbot/log.ts
@@ -1,8 +1,15 @@
+
+export type user_info = {
+    id: string,
+    name: string,
+}
 export class log {
     constructor (private module_name : string) { }
 
-    private format(msg : string | unknown) : string { return `[${this.module_name}]: ${msg}`; };
-    public info(msg : string) { console.log( this.format(msg) ); }
-    public warn(msg : string) { console.warn( this.format(msg) ); }
-    public error(msg : string | unknown) { console.error( this.format(msg) ); }
+    private format(msg : string | unknown, user? : user_info) : string { 
+        return `[${this.module_name}]: ${msg}` + (user ? ` [user_info: name=${user.name} id=${user.id}]` : ``); 
+    };
+    public info(msg : string, user? : user_info) { console.log( this.format(msg, user) ); }
+    public warn(msg : string, user? : user_info) { console.warn( this.format(msg, user) ); }
+    public error(msg : string | unknown, user? : user_info) { console.error( this.format(msg, user) ); }
 }
