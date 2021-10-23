fe57c98c06e666504cbeaa08ff112fa99a593cd1
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-10-23 18:35:53 +0300

    fix error for unknown type on log class

--- discordbot/log.ts
+++ discordbot/log.ts
@@ -3,5 +3,5 @@ export class log {
 
     public info(msg : string) { console.log( `${this.module_name}: ${msg}` ); }
     public warn(msg : string) { console.warn( `${this.module_name}: ${msg}` ); }
-    public error(msg : string) { console.error( `${this.module_name}: ${msg}` ); }
+    public error(msg : string | unknown) { console.error( `${this.module_name}: ${msg}` ); }
 }
