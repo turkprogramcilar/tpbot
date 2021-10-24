eabe6b0858780f74e23bbff804a19ddbe74a62c5
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-10-24 23:15:32 +0300

    shorten boilerplate code for modern modules

--- discordbot/modules/modern_boilerplate/main.ts
+++ discordbot/modules/modern_boilerplate/main.ts
@@ -1,12 +1,11 @@
 import { Message, MessageReaction, PartialUser, Presence, User } from "discord.js";
 import { dcmodule } from "../../module";
 
-const this_dcmodule = class modern_boilerplate extends dcmodule {
+export const m = new class modern_boilerplate extends dcmodule {
     
     constructor() { super(modern_boilerplate.name, false); }
     
-    public async after_init() {
-    }
+    public async after_init() { }
     public async on_message(msg : Message) {
 
         // if message is not prefixed, return
@@ -18,8 +17,4 @@ const this_dcmodule = class modern_boilerplate extends dcmodule {
     }
     public async on_reaction(reaction : MessageReaction, user : User | PartialUser) { }
     public async on_presence_update(old_p: Presence | undefined, new_p: Presence) { }
-}
-
-const this_instance = new this_dcmodule();
-export async function on_event(evt: string, args: any) { return this_instance.on_event(evt, args); }
-export async function init(refState: any) { return this_instance.init(refState); }
+};
--- discordbot/modules/welcome/main.ts
+++ discordbot/modules/welcome/main.ts
@@ -1,11 +1,5 @@
-import { Message, MessageReaction, PartialUser, Presence, User } from "discord.js";
 import { dcmodule } from "../../module";
-
-const this_dcmodule = class welcome extends dcmodule {
+export const m = new class welcome extends dcmodule {
     
     constructor() { super(welcome.name, false); }
 }
-
-const this_instance = new this_dcmodule();
-export async function on_event(evt: string, args: any) { return this_instance.on_event(evt, args); }
-export async function init(refState: any) { return this_instance.init(refState); }
