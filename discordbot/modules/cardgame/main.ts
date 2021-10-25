9be9323f41ce350163521c9c3ac9395047cddacc
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-10-25 10:21:20 +0300

    transform cardmaster main to modern module

--- discordbot/modules/cardgame/main.ts
+++ discordbot/modules/cardgame/main.ts
@@ -1,11 +1,6 @@
-import { Message, MessageReaction, PartialUser, Presence, User } from "discord.js";
 import { dcmodule } from "../../module";
 
-const this_dcmodule = class cardgame extends dcmodule {
+export const m = new class cardgame extends dcmodule {
     
     constructor() { super(cardgame.name, false); }
 }
-
-const this_instance = new this_dcmodule();
-export async function on_event(evt: string, args: any) { return this_instance.on_event(evt, args); }
-export async function init(refState: any) { return this_instance.init(refState); }
