c59f8fd1008ffec2dc51386768480fabe3c4eb77
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-10-30 21:50:20 +0300

    implement from method

--- tpbot/v2/Print.ts
+++ tpbot/v2/Print.ts
@@ -5,16 +5,23 @@ export type userInfo = {
 }
 export class Print
 {
-    private readonly log = console;
+    private _from: string | undefined = undefined;
+    private readonly _log = console;
     constructor(private name: string) { }
 
     private format(message: string | unknown, user?: userInfo): string
     {
-        return `[${this.name}]: ${message}` + (user ? ` [user_info: name=${user.name} id=${user.id}]` : ``);
+        return `[${this.name}]: ` + (this._from ? `(from=${this._from}) ` : ``) + `${message}` + (user ? ` (user_info: name=${user.name} id=${user.id})` : ``);
     };
-    public info(message: string, user?: userInfo) { this.log.log(this.format(message, user)); }
-    public warn(message: string, user?: userInfo) { this.log.warn(this.format(message, user)); }
-    public error(message: string | unknown, user?: userInfo) { this.log.error(this.format(message, user)); }
+
+    public from(name: string) {
+        this._from = name;
+        return this;
+    }
+
+    public info(message: string, user?: userInfo) { this._log.log(this.format(message, user)); }
+    public warn(message: string, user?: userInfo) { this._log.warn(this.format(message, user)); }
+    public error(message: string | unknown, user?: userInfo) { this._log.error(this.format(message, user)); }
 
     public exception(e: Error | unknown, subName?: string)
     {
