dc067278f81c16d9dbab0e3cd20c8182d37e6a38
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-11-04 23:21:01 +0300

    implement auto_thread module

---
+++ discordbot/modules/auto_topic/main.ts
@@ -0,0 +1,18 @@
+import { Message, MessageReaction, PartialUser, Presence, TextChannel, User } from "discord.js";
+import { dcmodule } from "../../module";
+
+export const m = new class auto_topic extends dcmodule {
+    
+    constructor() { super(auto_topic.name, false, false); }
+    
+    public async on_message(msg : Message) {
+
+        if (msg.channel instanceof TextChannel) {
+            
+            if (msg.channel.rateLimitPerUser == 120) {
+
+                msg.startThread({name: "👉Yorumlar"});
+            }
+        }
+    }
+};
