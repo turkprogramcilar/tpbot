597a180497e7a92137641269781e29c54f709911
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-11-06 15:11:51 +0300

    add main.ts

---
+++ discordbot/modules/sicardo/main.ts
@@ -0,0 +1,12 @@
+import { Message, MessageReaction, PartialUser, Presence, User } from "discord.js";
+import { modern } from "../../modern";
+
+export const m = new class pong extends modern {
+    
+    constructor() { super(pong.name); }
+    /* events */
+    public async on_message(msg: Message)
+    {
+        
+    }
+};
