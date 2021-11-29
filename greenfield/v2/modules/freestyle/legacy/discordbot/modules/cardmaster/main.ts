ddd34eb917d215a96c4519f081b1c664a97ab012
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-11-29 03:00:43 +0300

    clean up things

--- greenfield/v2/modules/freestyle/legacy/discordbot/modules/cardmaster/main.ts
+++ greenfield/v2/modules/freestyle/legacy/discordbot/modules/cardmaster/main.ts
@@ -1,6 +1,6 @@
 import { commander } from "../../commander";
 
-export const m = new class cardgame_master extends commander {
+export const m = new class cardmaster extends commander {
     
-    constructor() { super(cardgame_master.name, false); }
+    constructor() { super(cardmaster.name, false); }
 }();
