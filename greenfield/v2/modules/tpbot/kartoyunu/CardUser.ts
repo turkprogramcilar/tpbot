430f1a856aa1d14d9fce0d696749d31a1bcc3bd8
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-12-01 16:32:01 +0300

    improve carduser

--- greenfield/v2/modules/tpbot/kartoyunu/CardUser.ts
+++ greenfield/v2/modules/tpbot/kartoyunu/CardUser.ts
@@ -1,12 +1,17 @@
+import { User } from "discord.js";
 
 export class CardUser
 {
 /*******************************************************************72*/
-constructor(
-
-)
-{
-
+readonly id: string;
+constructor(id: string | User)
+{ 
+    if (id instanceof User) {
+        this.id = id.id;
+    }
+    else {
+        this.id = id;
+    }
 }
 
 /*******************************************************************72*/
